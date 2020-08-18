cf5397e8cf7243c8183044ce2d7e7707af49309b
MobSlicer152 <brambleclaw1414@gmail.com>
2020-08-18 13:25:41 +0000

    Fixed createiso.sh

--- createiso.sh
+++ createiso.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
-if [[ !-x xorriso ]]; then
+if [ !-x xorriso ]; then
 	printf "This script requires xorriso.\n"
+fi
 
-if [[ -e isosrc ]]; then
+if [ -e isosrc ]; then
 	rm -r isosrc
+fi
+
+if [ !-e $BUILDDIR/beefkern ]; then
+	cmake --build $BUILDDIR
 fi
 
 mkdir -p isosrc/boot/grub
@@ -12,7 +17,7 @@ mkdir -p isosrc/boot/grub
 printf "Copying kernel and GRUB config."
 cp grub.cfg isosrc/boot/grub
 printf "."
-cp build/beefkern isosrc/boot
+cp $BUILDDIR/beefkern isosrc/boot
 printf ".\n"
 
 grub-mkrescue isosrc -o beefos.iso
